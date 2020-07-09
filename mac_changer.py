4a2b58dab72ee9f0d458f286ecf4cc9175235b73
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2020-07-09 12:53:18 +0530

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 #import sys
 
 def get_arguments():
@@ -40,8 +41,28 @@ def change_mac(interface,new_mac):
     subprocess.call(['ifconfig', interface, 'down'])
     subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
     subprocess.call(['ifconfig', interface, 'up'])
-    subprocess.call(['ifconfig', interface])
+    #subprocess.call(['ifconfig', interface])
 
+def get_current_mac(interface):
+        ifconfig_result = subprocess.check_output(["ifconfig", interface])
+        #print(ifconfig_result)
+        mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+        if mac_address_search_result:
+            #print(mac_address_search_result.group(0))
+            return mac_address_search_result.group(0)
+        else:
+            print("[-] Could not read MAC address.")
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = "+ str(current_mac))
+
 change_mac(options.interface,options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to "+ current_mac)
+else:
+    print("[-] MAC address did not get changed.")
